25d9c3adad33e3cb9a302ba208b1514b64fb9581
michael miriti <michael@miriti.ru>
2016-09-02 22:37:16 +0300

    initial commit

---
+++ Source/Main.hx
@@ -0,0 +1,114 @@
+package;
+
+import openfl.events.Event;
+import openfl.events.KeyboardEvent;
+import openfl.display.Sprite;
+import openfl.display.FPS;
+import openfl.display.BitmapData;
+import openfl.display.Bitmap;
+import openfl.display.StageQuality;
+import openfl.geom.Rectangle;
+import openfl.Assets;
+import openfl.Lib;
+
+class Main extends Sprite {
+
+	var roadTexture:BitmapData;
+	var bitmapData:BitmapData;
+	var bitmap:Bitmap;
+	var cameraY:Int = 120;
+	var cameraX:Int = 0;
+	var scaling:Float = 1.0;
+	var cx:Int = Std.int(320 / 2);
+	var cy:Int = Std.int(240 / 2);
+
+	var z_shift:Float = 0;
+
+	public function new () {
+		super ();
+
+		Lib.current.stage.quality = StageQuality.LOW;
+
+		roadTexture = Assets.getBitmapData("assets/road.png");
+
+		bitmapData = new BitmapData(320, 240, false, 0xffffffff);
+
+		var fullRect = new Rectangle(0, 0, 320, 240);
+
+		bitmap = new Bitmap(bitmapData);
+		bitmap.scaleX = bitmap.scaleY = 2;
+		addChild(bitmap);
+
+		var fps = new FPS();
+		addChild(fps);
+
+		addEventListener(Event.ENTER_FRAME, onEnterFrame);
+
+		Lib.current.stage.addEventListener(KeyboardEvent.KEY_DOWN, function(e:KeyboardEvent):Void {
+			if(e.keyCode == 37) {
+				cameraX -= 2;
+			}
+
+			if(e.keyCode == 39) {
+				cameraX += 2;
+			}
+		});
+	}
+
+	function project_y(world_y:Float, world_z:Float) : Int {
+		return cy + Std.int(((world_y + cameraY) * scaling) / world_z);
+	}
+
+	function project(world_x:Float, world_y:Float, world_z:Float) : Array<Int> {
+		if(world_z == 0) {
+			world_z = 1;
+		}
+		return [
+			cx + Std.int(((world_x - cameraX) * scaling) / world_z),
+			cy + Std.int(((world_y + cameraY) * scaling) / world_z)
+		];
+	}
+
+	function fillRoadPoly(startY:Int, endY:Int, x1:Int, x2:Int, x3:Int, x4:Int, texture:BitmapData, y_offset:Float = 0) {
+		for(scanLine in startY...endY) {
+			if(scanLine >= 320) return;
+			var process:Float = (scanLine - startY) / (endY - startY);
+
+			var xStart = Std.int(x1 + (x3 - x1) * process);
+			var xEnd = Std.int(x2 + (x4 - x2) * process);
+
+			for(f_x in xStart...xEnd) {
+				var texture_x:Int = Std.int(texture.width * ((f_x - xStart) / (xEnd - xStart)));
+				var texture_y:Int = Std.int(texture.height * process);
+				var texturePixel = texture.getPixel(texture_x, texture_y);
+
+				bitmapData.setPixel32(f_x, scanLine, texturePixel);
+			}
+		}
+	}
+
+	function onEnterFrame(e:Event) : Void {
+		var segment_count = 25;
+
+		bitmapData.fillRect(new Rectangle(0, 0, 320, 240), 0x228dcb);
+		bitmapData.fillRect(new Rectangle(0, project_y(0, segment_count), 320, 120), 0x008833);
+
+		for(segment in 1...segment_count) {
+
+			var p1 = project(-160, 0, (segment_count - segment) + z_shift + 1);
+			var p2 = project(160, 0, (segment_count - segment) + z_shift + 1);
+			var p3 = project(-160, 0, (segment_count - segment) + z_shift);
+			var p4 = project(160, 0, (segment_count - segment) + z_shift);
+
+			fillRoadPoly(p1[1], p3[1], p1[0], p2[0], p3[0], p4[0], roadTexture);
+		}
+
+		z_shift -= 0.05;
+
+		while(z_shift < -1) {
+			z_shift += 1;
+		}
+	}
+
+
+}
